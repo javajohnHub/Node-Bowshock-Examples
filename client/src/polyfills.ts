cc4718d4699ccdf469902aa86a5c7fb1e6a5809a
John <joedw1978@gmail.com>
2018-08-28 15:43:23 -0400

    test

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -39,7 +39,7 @@
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-
+(window as any).global = window;
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
@@ -55,7 +55,7 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-(window as any).global = window;
+
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
