41faf3148ded9bbd81b401f4558bd961053e6c56
John <joedw1978@gmail.com>
2018-08-28 11:59:51 -0400

    formatting

--- server/sockets.js
+++ server/sockets.js
@@ -1,126 +1,98 @@
 module.exports = function(io) {
-let bowshock = require('node-bowshock');
-let rp = require('request-promise');
-    io.sockets.on('connection', function(socket) {
-       console.log('connected', socket.id)
+  let bowshock = require("node-bowshock");
+  let rp = require("request-promise");
+  io.sockets.on("connection", function(socket) {
+    console.log("connected", socket.id);
 
-        socket.on('get apod', (date) => {
-            console.log(date);
-            let formatted_date = format_date(date);
-            bowshock.apod(formatted_date)
-                .then((apod) => {
-                    socket.emit('send apod', apod)
-                })
-           
-            
-        })
-            socket.on('get curiosity', (date) => {
-                let formatted_date = format_date(date);
-                bowshock.mars.curiosity(formatted_date)
-                    .then((rover) => {
-                        socket.emit('send curiosity', rover)
-                    })
-               
-                
-            })
-
-                socket.on('get opportunity', (date) => {
-                    let formatted_date = format_date(date);
-                    bowshock.mars.opportunity(formatted_date)
-                        .then((rover) => {
-                            socket.emit('send opportunity',rover)
-                        })
-                              
-                })
-
-                    socket.on('get spirit', (date) => {
-                        let formatted_date = format_date(date);
-                        bowshock.mars.spirit(formatted_date)
-                            .then((rover) => {
-                                socket.emit('send spirit', rover)
-                            })
-                        
-            
-                        
-                    })
-        socket.on('get stats', () => {
-            let stats = bowshock.neows.stats();
-            console.log(stats)
-            socket.emit('send stats', stats)
-            .then((stats) => {
-                    socket.emit('send stats', stats)
-                })
-            
-           
-               
-            
-            
-        })
-
-        socket.on('get feed', (date) => {
-            let formatted_date = format_date(date);
-            bowshock.neows.feed(formatted_date)
-               .then((feed) => {
-                    socket.emit('send feed', feed)
-                })
-            
-        })
-        socket.on('get next', (url) => {
-            var options = {
-                uri: url,
-                json: true
-            };
-            rp(options)
-                .then(function (result) {
-                    socket.emit('send next', result)
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        })
-
-        socket.on('get previous', (url) => {
-            var options = {
-                uri: url,
-                json: true
-            };
-            rp(options)
-                .then(function (result) {
-                    socket.emit('send previous', result)
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        })
-
-        socket.on('get eva', () => {
-            bowshock.eva()
-               .then((eva) => {
-                    socket.emit('recieve eva', eva)
-                })
-          
-            
-        })
-
-        socket.on('get today', () => {
-            bowshock.neows.today()
-                .then((today) => {
-                    socket.emit('send today', today)
-                })
-           
-
-            
-        })
+    socket.on("get apod", date => {
+      console.log(date);
+      let formatted_date = format_date(date);
+      bowshock.apod(formatted_date).then(apod => {
+        socket.emit("send apod", apod);
+      });
+    });
+    socket.on("get curiosity", date => {
+      let formatted_date = format_date(date);
+      bowshock.mars.curiosity(formatted_date).then(rover => {
+        socket.emit("send curiosity", rover);
+      });
     });
 
-    function format_date(date) {
-        var year = date.year;
-        var month = date.month;
-        var day = date.day;
-        if(month < 10) month = '0' + month;
-        if(day < 10) day = '0' + day;
+    socket.on("get opportunity", date => {
+      let formatted_date = format_date(date);
+      bowshock.mars.opportunity(formatted_date).then(rover => {
+        socket.emit("send opportunity", rover);
+      });
+    });
 
-        return `${year}-${month}-${day}`;
+    socket.on("get spirit", date => {
+      let formatted_date = format_date(date);
+      bowshock.mars.spirit(formatted_date).then(rover => {
+        socket.emit("send spirit", rover);
+      });
+    });
+    socket.on("get stats", () => {
+      let stats = bowshock.neows.stats();
+      console.log(stats);
+      socket.emit("send stats", stats).then(stats => {
+        socket.emit("send stats", stats);
+      });
+    });
 
-    }
+    socket.on("get feed", date => {
+      let formatted_date = format_date(date);
+      bowshock.neows.feed(formatted_date).then(feed => {
+        socket.emit("send feed", feed);
+      });
+    });
+    socket.on("get next", url => {
+      var options = {
+        uri: url,
+        json: true
+      };
+      rp(options)
+        .then(function(result) {
+          socket.emit("send next", result);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    socket.on("get previous", url => {
+      var options = {
+        uri: url,
+        json: true
+      };
+      rp(options)
+        .then(function(result) {
+          socket.emit("send previous", result);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    socket.on("get eva", () => {
+      bowshock.eva().then(eva => {
+        socket.emit("recieve eva", eva);
+      });
+    });
+
+    socket.on("get today", () => {
+      bowshock.neows.today().then(today => {
+        socket.emit("send today", today);
+      });
+    });
+  });
+
+  function format_date(date) {
+    var year = date.year;
+    var month = date.month;
+    var day = date.day;
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    return `${year}-${month}-${day}`;
+  }
 };
