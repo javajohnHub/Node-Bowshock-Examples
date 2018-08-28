703af8c5578142984683ddb3206a9379f86bf9c3
John <joedw1978@gmail.com>
2018-08-28 12:23:28 -0400

    ??

--- server/app.js
+++ server/app.js
@@ -13,9 +13,9 @@ var app = express();
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, './server', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.use('/', index);
-
+app.use(cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
