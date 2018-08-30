96a61c3d5d59ddb25a7676d7d8f50305e5a8271a
John <joedw1978@gmail.com>
2018-08-30 01:20:40 -0400

    changes

--- client/src/app/components/apod/apod.component.ts
+++ client/src/app/components/apod/apod.component.ts
@@ -43,18 +43,13 @@ import {of} from 'rxjs';
   styles: [
     `
       .video-container {
+        position: absolute;
         padding-bottom: 56.25%;
         padding-top: 35px;
         height: 0;
         overflow: hidden;
       }
-      .video-container iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-      }
+      
     `
   ]
 })
