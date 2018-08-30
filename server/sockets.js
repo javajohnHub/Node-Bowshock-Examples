a74f74ec0d150b7030e7ab05c1ad1808438108f0
John <joedw1978@gmail.com>
2018-08-30 14:34:02 -0400

    b

--- server/sockets.js
+++ server/sockets.js
@@ -20,24 +20,24 @@ module.exports = function(io) {
     });
 
     socket.on("get manifest", rover => {
-      console.log(rover.rover)
+      
       if(!rover.sol && !rover.camera){
-        bowshock.mars.manifest(rover.rover).then(manifest => {
+        bowshock.mars.manifest(rover).then(manifest => {
           socket.emit("send manifest", manifest);
         });
       }
       if(rover.sol && !rover.camera){
-        bowshock.mars.manifest(rover.rover).then(manifest => {
+        bowshock.mars.manifest(rover).then(manifest => {
           socket.emit("send rover by sol", manifest);
         });
       }
       if(rover.camera && !rover.sol){
-        bowshock.mars.manifest(rover.rover).then(manifest => {
+        bowshock.mars.manifest(rover).then(manifest => {
           socket.emit("send rover by camera", manifest);
         });
       }
       if(rover.camera && rover.sol){
-        bowshock.mars.manifest(rover.rover).then(manifest => {
+        bowshock.mars.manifest(rover).then(manifest => {
           socket.emit("send rover by sol and camera", manifest);
         });
       }
