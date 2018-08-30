22a00a9a45a67ed534361a0343868c0cf80386c8
John <joedw1978@gmail.com>
2018-08-30 16:55:06 -0400

    1

--- client/src/app/components/mars/manifest/manifest.component.ts
+++ client/src/app/components/mars/manifest/manifest.component.ts
@@ -76,8 +76,8 @@ export class ManifestComponent {
       this.manifest = manifest.photo_manifest;
       this.photos = [];
       this.manifest.photos.forEach(photo => {
-        console.log(this.manifest.max_sol)
-        for(let i = 0; i < this.manifest.max_sol; i++){
+        console.log(this.manifest.max_sol, photo.total_photos)
+        for(let i = 0; i < photo.total_photos; i++){
           this.sols.push({ label: photo.sol, value: photo.sol });
           photo.cameras.forEach(camera => {
             this.cameras.push({ label: camera, value: camera });
