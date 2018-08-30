a4e299e0849c6727afee2f5086f546012d82a4ea
John <joedw1978@gmail.com>
2018-08-30 01:28:46 -0400

    changes

--- client/src/app/components/apod/apod.component.ts
+++ client/src/app/components/apod/apod.component.ts
@@ -5,7 +5,7 @@ import {of} from 'rxjs';
 @Component({
   selector: "app-apod",
   template: `    
-  <h1 class="center">Apod</h1>
+  <h1 class="ui-g ui-g-offset-6">Apod</h1>
     <div class="ui-g ui-fluid">
         <div class="ui-g-12">
             <p-calendar [showIcon]="true" [selectOtherMonths]="true" [readonlyInput]="true" (onSelect)="onDateChanged($event)" [(ngModel)]="model" dateFormat="yy-mm-dd" [maxDate]="maxDate"></p-calendar>
