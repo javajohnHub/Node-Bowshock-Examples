69a0b4612915edcbc2b67d20754c95dcfb908b5a
John <joedw1978@gmail.com>
2018-08-30 02:35:01 -0400

    c

--- server/sockets.js
+++ server/sockets.js
@@ -87,15 +87,16 @@ module.exports = function(io) {
   });
 
   function format_date(date) {
-    console.log(date)
+    if(typeof date == Date){
       var year = date.getFullYear();
-    var month = date.getMonth();
-    var day = date.getDay();
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
+      var month = date.getMonth();
+      var day = date.getDay();
+      if (month < 10) month = "0" + month;
+      if (day < 10) day = "0" + day;
+      return `${year}-${month}-${day}`;
+    }else{
+      return date;
+    }
 
-    
-    
-    return `${year}-${month}-${day}`;
   }
 };
