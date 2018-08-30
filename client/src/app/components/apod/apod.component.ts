b288d76ac3d8eb5f78d282e24a5a01a9e7248ff6
John <joedw1978@gmail.com>
2018-08-29 22:04:40 -0400

    changes

--- client/src/app/components/apod/apod.component.ts
+++ client/src/app/components/apod/apod.component.ts
@@ -69,12 +69,12 @@ export class ApodComponent {
         this.apod["url"]
       );
     });
-    this.socket.emit("get apod", this.model)
+    this.socket.emit("get apod", this.model.toISOString().split('T')[0])
   }
 
   onDateChanged(event): void {
     this.model = new Date(event);
-    this.socket.emit("get apod",new Date(this.model))
+    this.socket.emit("get apod",this.model.toISOString().split('T')[0])
       
      
   }
--- server/dist/main.67260925993e876efaa3.js
+++ server/dist/main.67260925993e876efaa3.js
@@ -71355,11 +71355,11 @@ var ApodComponent = /** @class */ (function () {
             _this.apod = data;
             _this.safe_url = _this.sanitizer.bypassSecurityTrustResourceUrl(_this.apod["url"]);
         });
-        this.socket.emit("get apod", this.model);
+        this.socket.emit("get apod", this.model.toISOString().split('T')[0]);
     };
     ApodComponent.prototype.onDateChanged = function (event) {
         this.model = new Date(event);
-        this.socket.emit("get apod", new Date(this.model));
+        this.socket.emit("get apod", this.model.toISOString().split('T')[0]);
     };
     return ApodComponent;
 }());
