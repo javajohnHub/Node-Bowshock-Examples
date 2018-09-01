2bc772ce767053571296412c8620229ab7e22df1
John <joedw1978@gmail.com>
2018-09-01 14:59:22 -0400

    m

--- server/sockets.js
+++ server/sockets.js
@@ -117,6 +117,7 @@ module.exports = function(io) {
       dateArr = date.split('-');
       console.log(dateArr[0], dateArr[1], dateArr[2])
       dateStr = "%4d/%02d/%02d" % (dateArr[0], dateArr[1], dateArr[2] )
+      console.log(dateStr)
       //dateStr = "{:4d}-{:02d}-{:02d}".format(dateArr[0], dateArr[1], dateArr[2] )
       return dateStr;
     }
