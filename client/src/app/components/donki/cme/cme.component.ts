2b69dc877210b65c230af3f2ca88c7a05f396027
John <joedw1978@gmail.com>
2018-09-03 14:51:29 -0400

    cme

--- client/src/app/components/donki/cme/cme.component.ts
+++ client/src/app/components/donki/cme/cme.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import { SocketService } from '../../../shared/socket.service';
+import { Component } from "@angular/core";
+import { SocketService } from "../../../shared/socket.service";
 
 @Component({
-  selector: 'app-cme',
-  templateUrl: 'cme.component.html'
+  selector: "app-cme",
+  templateUrl: "cme.component.html"
 })
 export class CMEComponent {
   socket: any;
-  constructor() {
-  }
+  cme;
+  constructor() {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.socket = SocketService.getInstance();
+
+    this.socket.on("send cme", cme => {
+      this.cme = cme;
+    });
   }
 }
