5e0667a62fb38b9222ccc5dfc57aef9b68c369a0
John <joedw1978@gmail.com>
2018-09-04 13:31:32 -0400

    initial form for options cmea

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,43 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {
-  LocationStrategy,
-  HashLocationStrategy
-} from '@angular/common';
-import {routing} from './app-routing.module';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { routing } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NoContentComponent } from './components/no-content/no-content';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ApodComponent } from './components/apod/apod.component';
-import {MarsComponent} from './components/mars/mars.component';
-import {CuriosityComponent} from './components/mars/curiosity/curiosity.component';
-import {SpiritComponent} from './components/mars/spirit/spirit.component';
-import {NeowsComponent} from './components/neows/neows.component';
-import {FeedComponent} from './components/neows/feed/feed.component';
-import {TodayComponent} from './components/neows/today/today.component';
+import { MarsComponent } from './components/mars/mars.component';
+import { CuriosityComponent } from './components/mars/curiosity/curiosity.component';
+import { SpiritComponent } from './components/mars/spirit/spirit.component';
+import { NeowsComponent } from './components/neows/neows.component';
+import { FeedComponent } from './components/neows/feed/feed.component';
+import { TodayComponent } from './components/neows/today/today.component';
 import { EvaComponent } from './components/eva/eva.component';
 import { ZippyComponent } from './components/zippy/zippy.component';
 
-import {CalendarModule} from 'primeng/calendar';
-import {GalleriaModule} from 'primeng/galleria';
+import { CalendarModule } from 'primeng/calendar';
+import { GalleriaModule } from 'primeng/galleria';
 import { OpportunityComponent } from './components/mars/opportunity/opportunity.component';
-import {CardModule} from 'primeng/card';
-import {PanelMenuModule} from 'primeng/panelmenu';
-import {DropdownModule} from 'primeng/dropdown'
+import { CardModule } from 'primeng/card';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { DropdownModule } from 'primeng/dropdown';
 import { ManifestComponent } from './components/mars/manifest/manifest.component';
-import {InputTextModule} from 'primeng/inputtext';
-import {DataScrollerModule} from 'primeng/datascroller';
-import {ButtonModule} from 'primeng/button';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
-import {AccordionModule} from 'primeng/accordion';
-import {DataViewModule} from 'primeng/dataview';
+import { InputTextModule } from 'primeng/inputtext';
+import { DataScrollerModule } from 'primeng/datascroller';
+import { ButtonModule } from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AccordionModule } from 'primeng/accordion';
+import { DataViewModule } from 'primeng/dataview';
 
-import {OrderListModule} from 'primeng/orderlist';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
+import { OrderListModule } from 'primeng/orderlist';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { DonkiComponent } from './components/donki/donki.component';
 import { CMEComponent } from './components/donki/cme/cme.component';
 import { CMEAComponent } from './components/donki/cmea/cmea.component';
@@ -51,58 +48,65 @@ import { SEPComponent } from './components/donki/sep/sep.component';
 import { WSASimComponent } from './components/donki/wsasim/wsasim.component';
 import { NotificationsComponent } from './components/donki/notifications/notifications.component';
 import { MyPanelMenuModule } from './shared/my-panel-menu';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { PanelModule } from 'primeng/panel';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NoContentComponent,
-    NavbarComponent,
-    ApodComponent,
-    MarsComponent,
-    CuriosityComponent,
-    OpportunityComponent,
-    SpiritComponent,
-    NeowsComponent,
-    FeedComponent,
-    EvaComponent,
-    TodayComponent,
-    ZippyComponent,
-    ManifestComponent,
-    DonkiComponent,
-    CMEComponent,
-    CMEAComponent,
-    FLRComponent,
-    GSTComponent,
-    HSSComponent,
-    IPSComponent,
-    MPCComponent,
-    RBEComponent,
-    SEPComponent,
-    WSASimComponent,
-    NotificationsComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    routing,
-    CalendarModule,
-    GalleriaModule,
-    CardModule,
-    PanelMenuModule,
-    DropdownModule,
-    InputTextModule,
-    DataScrollerModule,
-    ButtonModule,
-    ProgressSpinnerModule,
-    AccordionModule,
-    DataViewModule,
-    OrderListModule,
-    OverlayPanelModule,
-    MyPanelMenuModule
-  ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		NoContentComponent,
+		NavbarComponent,
+		ApodComponent,
+		MarsComponent,
+		CuriosityComponent,
+		OpportunityComponent,
+		SpiritComponent,
+		NeowsComponent,
+		FeedComponent,
+		EvaComponent,
+		TodayComponent,
+		ZippyComponent,
+		ManifestComponent,
+		DonkiComponent,
+		CMEComponent,
+		CMEAComponent,
+		FLRComponent,
+		GSTComponent,
+		HSSComponent,
+		IPSComponent,
+		MPCComponent,
+		RBEComponent,
+		SEPComponent,
+		WSASimComponent,
+		NotificationsComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		HttpClientModule,
+		routing,
+		CalendarModule,
+		GalleriaModule,
+		CardModule,
+		PanelMenuModule,
+		DropdownModule,
+		InputTextModule,
+		DataScrollerModule,
+		ButtonModule,
+		ProgressSpinnerModule,
+		AccordionModule,
+		DataViewModule,
+		OrderListModule,
+		OverlayPanelModule,
+		MyPanelMenuModule,
+		CheckboxModule,
+		InputSwitchModule,
+		ReactiveFormsModule,
+		PanelModule
+	],
+	providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/components/donki/cmea/cmea.component.ts
+++ client/src/app/components/donki/cmea/cmea.component.ts
@@ -1,104 +1,163 @@
-import { Component } from "@angular/core";
-import { SocketService } from "../../../shared/socket.service";
-import { SharedService } from "../../../shared/shared.service";
-import * as moment from "moment";
+import { Component } from '@angular/core';
+import { SocketService } from '../../../shared/socket.service';
+import { SharedService } from '../../../shared/shared.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import * as moment from 'moment';
+import { MenuItem, SelectItem } from 'primeng/api';
 
 export interface CMEA {
-  time21_5: string;
-  latitude: number;
-  longitude: number;
-  halfAngle: number;
-  speed: number;
-  type: string;
-  isMostAccurate: boolean;
-  associatedCMEID: string;
-  note: string;
-  catalog: string;
+	time21_5: string;
+	latitude: number;
+	longitude: number;
+	halfAngle: number;
+	speed: number;
+	type: string;
+	isMostAccurate: boolean;
+	associatedCMEID: string;
+	note: string;
+	catalog: string;
 }
 
 @Component({
-  selector: "app-cmea",
-  templateUrl: "cmea.component.html"
+	selector: 'app-cmea',
+	templateUrl: 'cmea.component.html'
 })
 export class CMEAComponent {
-  socket: any;
-  cmea: CMEA[];
-  startModel: Date = new Date(
-    moment()
-      .subtract(30, "days")
-      .format()
-  );
-  endModel: Date = new Date(
-    moment()
-      .subtract(30, "days")
-      .format()
-  );
-  maxStartDate: Date = new Date(
-    moment()
-      .subtract(30, "days")
-      .format()
-  );
-  maxEndDate: Date = new Date(
-    moment()
-      .subtract(30, "days")
-      .format()
-  );
-  isLoading: boolean = false;
-  constructor(private _sharedService: SharedService) {}
+	socket: any;
+	cmea: CMEA[];
+	cmeaForm: FormGroup;
+	mostAccurateOnlyToggle: boolean = true;
+	completeEntryOnlyToggle: boolean = true;
+	selectedCatalog: string;
+	startModel: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	endModel: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	maxStartDate: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	maxEndDate: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	isLoading: boolean = false;
+	catalogs: SelectItem[];
+	constructor(private _sharedService: SharedService) {}
 
-  ngOnInit() {
-    this.isLoading = true;
-    this._sharedService.subTitleSubject$.next(
-      "DONKI/Coronal Mass Ejection Analysis"
-    );
-    this.socket = SocketService.getInstance();
-    this.socket.on("send cmea", cmea => {
-      this.cmea = cmea;
-      this.isLoading = false;
-    });
+	ngOnInit() {
+		this.isLoading = true;
+		this.catalogs = [
+			{ label: 'Select Catalog', value: 'ALL' },
+			{ label: 'ALL', value: 'ALL' },
+			{ label: 'SWRC_CATALOG', value: 'SWRC_CATALOG' },
+			{ label: 'JANG_ET_AL_CATALOG', value: 'JANG_ET_AL_CATALOG' }
+		];
+		this._createForm();
+		this._sharedService.subTitleSubject$.next(
+			'DONKI/Coronal Mass Ejection Analysis'
+		);
+		this.socket = SocketService.getInstance();
+		this.socket.on('send cmea', cmea => {
+			this.cmea = cmea;
+			this.isLoading = false;
+		});
 
-    this.socket.emit("get cmea", {
-      startDate: moment(this.startModel).format("YYYY-MM-DD")
-    });
-  }
+		this.socket.emit('get cmea', {
+			startDate: moment(this.startModel).format('YYYY-MM-DD')
+		});
+	}
 
-  goToAssoc(date) {
-    this.isLoading = true;
+	private _createForm() {
+		this.cmeaForm = new FormGroup({
+			keyword: new FormControl(''),
+			speed: new FormControl(0),
+			halfAngle: new FormControl(0),
+			catalogs: new FormControl(''),
+			complete: new FormControl(true),
+			accurate: new FormControl(true)
+		});
+	}
 
-    let newDate = date.split("T");
-    this.startModel = new Date(moment(newDate[0]).format("YYYY-MM-DD"));
-    this.socket.emit("get cmea", {
-      startDate: moment(newDate[0]).format("YYYY-MM-DD")
-    });
-  }
+	completeChanged(event) {
+		console.log('complete', event);
+	}
+	accurateChanged(event) {
+		console.log('accurate', event);
+	}
 
-  setCMEADate() {
-    this.isLoading = true;
-    if (
-      moment(this.startModel).format("YYYY-MM-DD") ==
-      moment(this.endModel).format("YYYY-MM-DD")
-    ) {
-      this.socket.emit("get cmea", {
-        startDate: moment(this.startModel).format("YYYY-MM-DD")
-      });
-    }
-    if (
-      moment(this.startModel).format("YYYY-MM-DD") >
-      moment(this.endModel).format("YYYY-MM-DD")
-    ) {
-      this.endModel = new Date(
-        moment()
-          .subtract(30, "days")
-          .format()
-      );
-      this.socket.emit("get cmea", {
-        startDate: moment(this.startModel).format("YYYY-MM-DD")
-      });
-    } else {
-      this.socket.emit("get cmea", {
-        startDate: moment(this.startModel).format("YYYY-MM-DD"),
-        endDate: moment(this.endModel).format("YYYY-MM-DD")
-      });
-    }
-  }
+	catalogSelected(catalog) {
+		console.log('catalog', catalog);
+	}
+	goToAssoc(date) {
+		this.isLoading = true;
+
+		let newDate = date.split('T');
+		this.startModel = new Date(moment(newDate[0]).format('YYYY-MM-DD'));
+		this.socket.emit('get cmea', {
+			startDate: moment(newDate[0]).format('YYYY-MM-DD')
+		});
+	}
+
+	setCMEADate() {
+		this.isLoading = true;
+		if (
+			moment(this.startModel).format('YYYY-MM-DD') ==
+			moment(this.endModel).format('YYYY-MM-DD')
+		) {
+			this.socket.emit('get cmea', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+		}
+		if (
+			moment(this.startModel).format('YYYY-MM-DD') >
+			moment(this.endModel).format('YYYY-MM-DD')
+		) {
+			this.endModel = new Date(
+				moment()
+					.subtract(30, 'days')
+					.format()
+			);
+			this.socket.emit('get cmea', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+		} else {
+			this.socket.emit('get cmea', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD'),
+				endDate: moment(this.endModel).format('YYYY-MM-DD')
+			});
+		}
+	}
+
+	get keyword() {
+		return this.cmeaForm.get('keyword');
+	}
+
+	get speed() {
+		return this.cmeaForm.get('speed');
+	}
+
+	get halfAngle() {
+		return this.cmeaForm.get('halfAngle');
+	}
+
+	get complete() {
+		return this.cmeaForm.get('completeEntryOnly');
+	}
+
+	get accurate() {
+		return this.cmeaForm.get('mostAccurateOnly');
+	}
+
+	get catalogsControl() {
+		return this.cmeaForm.get('catalogs');
+	}
 }
