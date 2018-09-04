ac51b93aa03f4a01d82578d77652fae240081c61
John <joedw1978@gmail.com>
2018-09-04 19:03:41 -0400

    solar flare finished

--- client/src/app/components/donki/flr/flr.component.ts
+++ client/src/app/components/donki/flr/flr.component.ts
@@ -3,23 +3,92 @@ import { SocketService } from '../../../shared/socket.service';
 import { SharedService } from '../../../shared/shared.service';
 import * as moment from 'moment';
 @Component({
-  selector: 'app-flr',
-  templateUrl: 'flr.component.html'
+	selector: 'app-flr',
+	templateUrl: 'flr.component.html'
 })
 export class FLRComponent {
-  socket: any;
-  flr;
-  constructor(private _sharedService: SharedService) {
-  }
+	socket: any;
+	flr: any; //FLR[];
+	startModel: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	endModel: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	maxStartDate: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	maxEndDate: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	isLoading: boolean = false;
+	cme;
+	constructor(private _sharedService: SharedService) {}
 
-  ngOnInit() {
-    this._sharedService.subTitleSubject$.next('DONKI/Solar Flare')
-    this.socket = SocketService.getInstance();
+	ngOnInit() {
+		this.isLoading = true;
+		this._sharedService.subTitleSubject$.next('DONKI/Solar Flare');
+		this.socket = SocketService.getInstance();
+		this.socket.on('send flr', flr => {
+			this.flr = flr;
+			this.cme = null;
+			this.isLoading = false;
+		});
+		this.socket.on('send cme', cme => {
+			this.cme = cme;
+			this.flr = null;
+			this.isLoading = false;
+		});
+		this.socket.emit('get flr', {
+			startDate: moment(this.startModel).format('YYYY-MM-DD')
+		});
+	}
 
-    this.socket.on("send flr", flr => {
-      this.flr = flr;
-    });
+	goToAssoc(date) {
+		this.isLoading = true;
 
-    this.socket.emit('get flr');
-  }
+		let newDate = date.split('T');
+		this.startModel = new Date(moment(newDate[0]).format('YYYY-MM-DD'));
+		this.socket.emit('get cme', {
+			startDate: moment(newDate[0]).format('YYYY-MM-DD')
+		});
+	}
+
+	setFLRDate() {
+		this.isLoading = true;
+		if (
+			moment(this.startModel).format('YYYY-MM-DD') ==
+			moment(this.endModel).format('YYYY-MM-DD')
+		) {
+			this.socket.emit('get flr', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+		}
+		if (
+			moment(this.startModel).format('YYYY-MM-DD') >
+			moment(this.endModel).format('YYYY-MM-DD')
+		) {
+			this.endModel = new Date(
+				moment()
+					.subtract(30, 'days')
+					.format()
+			);
+			this.socket.emit('get flr', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+		} else {
+			this.socket.emit('get flr', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD'),
+				endDate: moment(this.endModel).format('YYYY-MM-DD')
+			});
+		}
+	}
 }
