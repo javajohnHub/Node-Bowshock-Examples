d7fb9276a33cb65293e337aca7f6a4085a51d0d2
John <joedw1978@gmail.com>
2018-09-04 11:03:12 -0400

    cmea complete

--- client/src/app/components/donki/cme/cme.component.ts
+++ client/src/app/components/donki/cme/cme.component.ts
@@ -1,76 +1,3 @@
-// import { Component } from "@angular/core";
-// import { SocketService } from "../../../shared/socket.service";
-// import { SharedService } from "../../../shared/shared.service";
-
-// export interface Instruments {
-//   id: number;
-//   displayName: string;
-// }
-
-// export interface CMEAnalyses {
-//   time21_5: string;
-//   latitude: number;
-//   longitude: number;
-//   halfAngle: number;
-//   speed: number;
-//   type: string;
-//   isMostAccurate: boolean;
-//   note: string;
-//   levelOfData: number;
-//   enlilList: any;
-// }
-// export interface CME {
-//   activityId: Date;
-//   startTime: Date;
-//   sourceLocation: string;
-//   activeRegionNum: number;
-//   instruments: Instruments[];
-//   cmeAnalyses: CMEAnalyses[];
-//   linkedEvents: any;
-//   note: string;
-//   catalog: string;
-// }
-// @Component({
-//   selector: "app-cme",
-//   templateUrl: "cme.component.html"
-// })
-// export class CMEComponent {
-//   socket: any;
-//   cme: CME;
-//   startModel: Date;
-//   endModel: Date;
-//   constructor(private _sharedService: SharedService) {}
-
-//   ngOnInit() {
-//     this._sharedService.subTitleSubject$.next('Space Weather Database Of Notifications, Knowledge, Information/Coronal Mass Ejection')
-//     this.socket = SocketService.getInstance();
-//     this.startModel = new Date();
-//     this.endModel = new Date(this.startModel);
-//     this.socket.on("send cme", cme => {
-//       this.cme = cme;
-//     });
-//     let myDate = this.startModel.toISOString().split("T")[0];
-//     let last = parseInt(myDate.split("-")[2]);
-//     let str = myDate.split("-")[0] + "-" + myDate.split("-")[1] + "-" + last;
-//     this.socket.emit("get cme", { startDate: str });
-//   }
-
-//   getCME(start, end) {
-//     if (end == null) {
-//       this.startModel = new Date(start);
-//       let myDate = this.startModel.toISOString().split("T")[0];
-//       this.socket.emit("get cme", { startDate: myDate });
-//     } else {
-//       this.startModel = new Date(start);
-//       this.endModel = new Date(end);
-//       let myStart = this.startModel.toISOString().split("T")[0];
-//       let myEnd = this.endModel.toISOString().split("T")[0];
-//       this.socket.emit("get cme", { startDate: myStart, endDate: myEnd });
-//     }
-//   }
-// }
-
-
 import { Component } from "@angular/core";
 import { SocketService } from "../../../shared/socket.service";
 import { SharedService } from "../../../shared/shared.service";
@@ -114,18 +41,22 @@ export class CMEComponent {
   endModel: Date = new Date(moment().subtract(30, 'days').format());
   maxStartDate: Date = new Date(moment().subtract(30, 'days').format());
   maxEndDate: Date = new Date(moment().subtract(30, 'days').format());
+  isLoading: boolean = false;
   constructor(private _sharedService: SharedService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this._sharedService.subTitleSubject$.next('DONKI/Coronal Mass Ejection')
     this.socket = SocketService.getInstance();
     this.socket.on("send cme", cme => {
       this.cme = cme;
+      this.isLoading = false;
     });
     this.socket.emit("get cme", { startDate: moment(this.startModel).format('YYYY-MM-DD') });
   }
 
   setCMEDate() {
+    this.isLoading = true;
     if(moment(this.startModel).format('YYYY-MM-DD') ==  moment(this.endModel).format('YYYY-MM-DD')){
       this.socket.emit("get cme", { startDate: moment(this.startModel).format('YYYY-MM-DD')});
     }
@@ -133,9 +64,9 @@ export class CMEComponent {
       this.endModel = new Date(moment().subtract(30, 'days').format());
       this.socket.emit("get cme", { startDate: moment(this.startModel).format('YYYY-MM-DD')});
     }else{
-      this.socket.emit("get cme", { startDate: moment(this.startModel).format('YYYY-MM-DD'), endDate: moment(this.endModel).format('YYYY-MM-DD')});
+      this.socket.emit("get cme", { startDate: moment(this.startModel).format('YYYY-MM-DD'),
+       endDate: moment(this.endModel).format('YYYY-MM-DD')});
     }
-    
   }
 }
 
--- client/src/app/components/donki/cmea/cmea.component.ts
+++ client/src/app/components/donki/cmea/cmea.component.ts
@@ -1,25 +1,104 @@
-import { Component } from '@angular/core';
-import { SocketService } from '../../../shared/socket.service';
-import { SharedService } from '../../../shared/shared.service';
-import * as moment from 'moment';
+import { Component } from "@angular/core";
+import { SocketService } from "../../../shared/socket.service";
+import { SharedService } from "../../../shared/shared.service";
+import * as moment from "moment";
+
+export interface CMEA {
+  time21_5: string;
+  latitude: number;
+  longitude: number;
+  halfAngle: number;
+  speed: number;
+  type: string;
+  isMostAccurate: boolean;
+  associatedCMEID: string;
+  note: string;
+  catalog: string;
+}
+
 @Component({
-  selector: 'app-cmea',
-  templateUrl: 'cmea.component.html'
+  selector: "app-cmea",
+  templateUrl: "cmea.component.html"
 })
 export class CMEAComponent {
   socket: any;
-  cmea;
-  constructor(private _sharedService: SharedService) {
-  }
+  cmea: CMEA[];
+  startModel: Date = new Date(
+    moment()
+      .subtract(30, "days")
+      .format()
+  );
+  endModel: Date = new Date(
+    moment()
+      .subtract(30, "days")
+      .format()
+  );
+  maxStartDate: Date = new Date(
+    moment()
+      .subtract(30, "days")
+      .format()
+  );
+  maxEndDate: Date = new Date(
+    moment()
+      .subtract(30, "days")
+      .format()
+  );
+  isLoading: boolean = false;
+  constructor(private _sharedService: SharedService) {}
 
   ngOnInit() {
-    this._sharedService.subTitleSubject$.next('Space Weather Database Of Notifications, Knowledge, Information/Coronal Mass Ejection Analysis')
+    this.isLoading = true;
+    this._sharedService.subTitleSubject$.next(
+      "DONKI/Coronal Mass Ejection Analysis"
+    );
     this.socket = SocketService.getInstance();
-
     this.socket.on("send cmea", cmea => {
       this.cmea = cmea;
+      this.isLoading = false;
     });
 
-    this.socket.emit('get cmea');
+    this.socket.emit("get cmea", {
+      startDate: moment(this.startModel).format("YYYY-MM-DD")
+    });
+  }
+
+  goToAssoc(date) {
+    this.isLoading = true;
+
+    let newDate = date.split("T");
+    this.startModel = new Date(moment(newDate[0]).format("YYYY-MM-DD"));
+    this.socket.emit("get cmea", {
+      startDate: moment(newDate[0]).format("YYYY-MM-DD")
+    });
+  }
+
+  setCMEADate() {
+    this.isLoading = true;
+    if (
+      moment(this.startModel).format("YYYY-MM-DD") ==
+      moment(this.endModel).format("YYYY-MM-DD")
+    ) {
+      this.socket.emit("get cmea", {
+        startDate: moment(this.startModel).format("YYYY-MM-DD")
+      });
+    }
+    if (
+      moment(this.startModel).format("YYYY-MM-DD") >
+      moment(this.endModel).format("YYYY-MM-DD")
+    ) {
+      this.endModel = new Date(
+        moment()
+          .subtract(30, "days")
+          .format()
+      );
+      this.socket.emit("get cmea", {
+        startDate: moment(this.startModel).format("YYYY-MM-DD")
+      });
+    } else {
+      this.socket.emit("get cmea", {
+        startDate: moment(this.startModel).format("YYYY-MM-DD"),
+        endDate: moment(this.endModel).format("YYYY-MM-DD")
+      });
+    }
   }
 }
