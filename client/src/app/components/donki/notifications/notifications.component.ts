5421744353d90a149e6aec625a18965974f33aa4
John <joedw1978@gmail.com>
2018-09-06 23:44:29 -0400

    DONKI all connections made

--- client/src/app/components/donki/notifications/notifications.component.ts
+++ client/src/app/components/donki/notifications/notifications.component.ts
@@ -1,25 +1,94 @@
 import { Component } from '@angular/core';
 import { SocketService } from '../../../shared/socket.service';
 import { SharedService } from '../../../shared/shared.service';
+import { FormGroup, FormControl } from '@angular/forms';
 import * as moment from 'moment';
+import { SelectItem } from 'primeng/api';
+
 @Component({
-  selector: 'app-notifications',
-  templateUrl: 'notifications.component.html'
+	selector: 'app-notifications',
+	templateUrl: 'notifications.component.html'
 })
 export class NotificationsComponent {
-  socket: any;
-  notifications;
-  constructor(private _sharedService: SharedService) {
-  }
+	socket: any;
+	notifications: any;
+	startModel: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	endModel: Date = new Date();
+	maxStartDate: Date = new Date(
+		moment()
+			.subtract(30, 'days')
+			.format()
+	);
+	maxEndDate: Date = new Date();
+	isLoading: boolean = false;
+	constructor(private _sharedService: SharedService) {}
 
-  ngOnInit() {
-    this._sharedService.subTitleSubject$.next('DONKI/Notifications')
-    this.socket = SocketService.getInstance();
+	ngOnInit() {
+		this.isLoading = true;
+		this._sharedService.subTitleSubject$.next('Notifications');
+		this.socket = SocketService.getInstance();
+		this.socket.on('send notifications', notifications => {
+			this.notifications = notifications;
+			this.isLoading = false;
+		});
 
-    this.socket.on("send notifications", notifications => {
-      this.notifications = notifications;
-    });
+		this.socket.emit('get notifications', {
+			startDate: moment(this.startModel).format('YYYY-MM-DD')
+		});
+	}
 
-    this.socket.emit('get notifications');
-  }
+	goToAssoc(date) {
+		this.isLoading = true;
+
+		let newDate = date.split('T');
+		let type = date.split('-');
+		this.startModel = new Date(moment(newDate[0]).format('YYYY-MM-DD'));
+		this.socket.emit('get ' + type[3].toLowerCase(), {
+			startDate: moment(this.startModel).format('YYYY-MM-DD')
+		});
+	}
+
+	setOptions() {
+		if (
+			moment(this.startModel).format('YYYY-MM-DD') ==
+			moment(this.endModel).format('YYYY-MM-DD')
+		) {
+			console.log('1');
+			this.socket.emit('get notifications', {
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+			console.log({
+				startDate: moment(this.startModel).format('YYYY-MM-DD')
+			});
+		} else {
+			if (
+				moment(this.startModel).format('YYYY-MM-DD') >
+				moment(this.endModel).format('YYYY-MM-DD')
+			) {
+				this.endModel = new Date();
+				console.log('2');
+				this.socket.emit('get notifications', {
+					startDate: moment(this.startModel).format('YYYY-MM-DD')
+				});
+				console.log({
+					startDate: moment(this.startModel).format('YYYY-MM-DD'),
+					endDate: moment(this.endModel).format('YYYY-MM-DD')
+				});
+			} else {
+				console.log('else');
+				this.socket.emit('get notifications', {
+					startDate: moment(this.startModel).format('YYYY-MM-DD'),
+					endDate: moment(this.endModel).format('YYYY-MM-DD')
+				});
+				console.log({
+					startDate: moment(this.startModel).format('YYYY-MM-DD'),
+					endDate: moment(this.endModel).format('YYYY-MM-DD')
+				});
+			}
+		}
+	}
 }
