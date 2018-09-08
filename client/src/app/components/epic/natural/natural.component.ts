1cffbe1fa6936c47c23607fb35d35b741d76c676
John <joedw1978@gmail.com>
2018-09-08 19:22:13 -0400

    natural changes

--- client/src/app/components/epic/natural/natural.component.ts
+++ client/src/app/components/epic/natural/natural.component.ts
@@ -22,36 +22,39 @@ export class NaturalComponent {
 		this.socket = SocketService.getInstance();
 		this.socket.on('send natural all', data => {
 			this.naturalAll = data;
-			this.natural = null;
-			this.naturalByDate = null;
-			this.naturalAvailable = null;
+			// this.natural = null;
+			// this.naturalByDate = null;
+			// this.naturalAvailable = null;
 		});
 		this.socket.on('send natural by date', data => {
 			this.naturalByDate = data;
-			this.naturalAll = null;
-			this.natural = null;
-			this.naturalAvailable = null;
+			// this.naturalAll = null;
+			// this.natural = null;
+			// this.naturalAvailable = null;
 		});
 
 		this.socket.on('send natural', data => {
 			this.natural = data;
-			this.naturalAll = null;
-			this.naturalByDate = null;
-			this.naturalAvailable = null;
+			// this.naturalAll = null;
+			// this.naturalByDate = null;
+			// this.naturalAvailable = null;
 		});
 
 		this.socket.on('send natural available', data => {
 			this.naturalAvailable = data;
-			this.natural = null;
-			this.naturalAll = null;
-			this.naturalByDate = null;
+			// this.natural = null;
+			// this.naturalAll = null;
+			// this.naturalByDate = null;
 		});
 		let myDate = this.model.toISOString().split('T')[0];
 		let last = parseInt(myDate.split('-')[2]) - 1;
 		let str =
 			myDate.split('-')[0] + '-' + myDate.split('-')[1] + '-' + last;
 
-		this.socket.emit('get natural available', str);
+		this.socket.emit('get natural');
+		this.socket.emit('get natural all');
+		this.socket.emit('get natural available');
+		this.socket.emit('get natural by date', str);
 	}
 	onDateChanged(event): void {
 		this.model = new Date(event);
