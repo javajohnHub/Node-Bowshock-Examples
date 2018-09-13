29cd1b08f5f735586f9aa4c3a4e8c913f59a6b62
John <joedw1978@gmail.com>
2018-09-13 14:46:12 -0400

    search

--- client/src/app/components/vid-and-image/search/search.component.ts
+++ client/src/app/components/vid-and-image/search/search.component.ts
@@ -23,6 +23,6 @@ export class SearchComponent implements OnInit {
 	}
 	getData() {
 		this.isLoading = true;
-		this.socket.emit('get media', { target: this.model });
+		this.socket.emit('get media', { query: this.model });
 	}
 }
