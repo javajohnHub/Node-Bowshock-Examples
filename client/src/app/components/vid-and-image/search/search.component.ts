e2428bb1c3e6d0d20952b4ef08e6c686ff0637c0
John <joedw1978@gmail.com>
2018-09-13 16:18:18 -0400

    version

--- client/src/app/components/vid-and-image/search/search.component.ts
+++ client/src/app/components/vid-and-image/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../../shared/shared.service';
 import { SocketService } from '../../../shared/socket.service';
-
+import VTTConverter from 'srt-webvtt';
+import { HttpClient } from '@angular/common/http';
 @Component({
 	selector: 'app-search',
 	templateUrl: './search.component.html'
@@ -11,13 +12,58 @@ export class SearchComponent implements OnInit {
 	isLoading: boolean;
 	media;
 	model = 'apollo';
-	constructor(private _sharedService: SharedService) {}
+	hrefs = [];
+	srts = [];
+	mp4s = [];
+	mp4;
+	pngs = [];
+	jsons = [];
+	constructor(
+		private _sharedService: SharedService,
+		private _http: HttpClient
+	) {}
 
 	ngOnInit() {
 		this._sharedService.subTitleSubject$.next('Image and Video Search');
 		this.socket = SocketService.getInstance();
 		this.socket.on('send media', media => {
-			this.media = media;
+			this.media = media.collection.items;
+			this.media.forEach(med => {
+				this.hrefs.push(med.href);
+			});
+			this.hrefs.forEach(href => {
+				this._http
+					.get(href)
+					.pipe()
+					.subscribe(data => {
+						this.srts.push(data);
+					});
+				if (this.srts[0]) {
+					this.srts[0].forEach(url => {
+						if (url.slice(-3) == 'mp4') {
+							this.mp4s.push(url);
+							console.log('mp4', url);
+						}
+						if (this.mp4s) {
+							this.mp4s.forEach(mp4 => {
+								this.mp4 = mp4.split('~');
+								if (this.mp4[1] == 'medium.mp4') {
+									this.mp4s.push(mp4);
+								}
+							});
+						}
+						if (url.slice(-3) == 'png' || url.slice(-3) == 'jpg') {
+							this.pngs.push(url);
+							console.log('png', url);
+						}
+						if (url.slice(-4) == 'json') {
+							this.jsons.push(url);
+							console.log('json', url);
+						}
+					});
+				}
+			});
+
 			this.isLoading = false;
 		});
 	}
