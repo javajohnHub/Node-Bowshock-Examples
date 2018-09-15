98cff9daca273ba0f77d6196218cd8b523a6e5d8
John <joedw1978@gmail.com>
2018-09-15 14:08:32 -0400

    changes

--- client/src/app/components/eonet/events/event/event.component.ts
+++ client/src/app/components/eonet/events/event/event.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { SharedService } from '../../../../shared/shared.service';
 import { SocketService } from '../../../../shared/socket.service';
 
 @Component({
@@ -7,21 +6,17 @@ import { SocketService } from '../../../../shared/socket.service';
 	templateUrl: 'event.component.html'
 })
 export class EventComponent {
-	socket: any;
+	socket = SocketService.getInstance();
 	@Input('event')
 	event: any;
 	imageUrl: any = [];
 	isLoading: boolean = false;
 	geos = [];
 	tileDimension = 0.145;
-	constructor(private _sharedService: SharedService) {}
+	constructor() {}
 
 	ngOnInit() {
-		this._sharedService.subTitleSubject$.next(
-			'The Earth Observatory Natural Event Tracker/Event'
-		);
 		this.isLoading = true;
-		this.socket = SocketService.getInstance();
 
 		this.socket.on('send earth imagery', imageUrl => {
 			this.imageUrl = imageUrl;
@@ -31,10 +26,14 @@ export class EventComponent {
 
 	getImages(geo) {
 		this.isLoading = true;
-		this.socket.emit('get earth imagery', {
-			lon: geo.coordinates[0],
-			lat: geo.coordinates[1],
-			dim: this.tileDimension
-		});
+		this.socket.emit(
+			'get earth imagery',
+			{
+				lon: geo.coordinates[0],
+				lat: geo.coordinates[1],
+				dim: this.tileDimension
+			},
+			data => console.log(data)
+		);
 	}
 }
