eaea5974ac4901f5bc064e625cc2f1c7e8d54264
John <joedw1978@gmail.com>
2018-09-15 21:20:44 -0400

    img video apod

--- client/src/app/components/apod/apod.component.ts
+++ client/src/app/components/apod/apod.component.ts
@@ -1,50 +1,50 @@
-import { Component } from "@angular/core";
-import { SocketService } from "../../shared/socket.service";
-import { DomSanitizer } from "@angular/platform-browser";
-import { SharedService } from "../../shared/shared.service";
+import { Component } from '@angular/core';
+import { SocketService } from '../../shared/socket.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SharedService } from '../../shared/shared.service';
 
 @Component({
-  selector: "app-apod",
-  templateUrl: 'apod.component.html',
-  styles: [
-`
-      .video-container iframe {
-        
-        width: 100%;
-        height: 100%
-      }
-    `
-  ]
+	selector: 'app-apod',
+	templateUrl: 'apod.component.html'
 })
 export class ApodComponent {
-  socket: any;
-  apod: {};
-  safe_url: any;
-  model: Date;
-  maxDate: Date;
-  constructor(private sanitizer: DomSanitizer, private _sharedService: SharedService) {
-  }
+	socket: any;
+	apod: {};
+	safe_url: any;
+	model: Date;
+	maxDate: Date;
+	constructor(
+		private sanitizer: DomSanitizer,
+		private _sharedService: SharedService
+	) {}
 
-  ngOnInit(){
-    this._sharedService.subTitleSubject$.next('Astronomy Picture of the Day')
-    this.model = new Date();
-    this.socket = SocketService.getInstance();
-    this.socket.on("send apod", data => {
-      this.apod = data;
-      this.safe_url = this.sanitizer.bypassSecurityTrustResourceUrl(
-        this.apod["url"]
-      );
-    });
-    let myDate = this.model.toISOString().split('T')[0]
-    let last = parseInt(myDate.split('-')[2]);
-    let str = myDate.split('-')[0] + '-' + myDate.split('-')[1] + '-' + last;
-    this.maxDate = new Date(myDate.split('-')[0] + '-' + myDate.split('-')[1] + '-' + last)
-    this.socket.emit('get apod', str );
-  }
+	ngOnInit() {
+		this._sharedService.subTitleSubject$.next(
+			'Astronomy Picture of the Day'
+		);
+		this.model = new Date();
+		this.socket = SocketService.getInstance();
+		this.socket.on('send apod', data => {
+			this.apod = data;
+			if (this.apod) {
+				this.safe_url = this.sanitizer.bypassSecurityTrustResourceUrl(
+					this.apod['url']
+				);
+			}
+		});
+		let myDate = this.model.toISOString().split('T')[0];
+		let last = parseInt(myDate.split('-')[2]);
+		let str =
+			myDate.split('-')[0] + '-' + myDate.split('-')[1] + '-' + last;
+		this.maxDate = new Date(
+			myDate.split('-')[0] + '-' + myDate.split('-')[1] + '-' + last
+		);
+		this.socket.emit('get apod', str);
+	}
 
-  onDateChanged(event): void {
-    this.model = new Date(event);
-    let myDate = this.model.toISOString().split('T')[0]
-    this.socket.emit("get apod", myDate)
-  }
+	onDateChanged(event): void {
+		this.model = new Date(event);
+		let myDate = this.model.toISOString().split('T')[0];
+		this.socket.emit('get apod', myDate);
+	}
 }
