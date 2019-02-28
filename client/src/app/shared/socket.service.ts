0cd49b8529314154e9a4bdd437d080226c71d9b2
javajohn <john.a.edwards@basf.com>
2019-02-28 13:23:09 -0500

    dgjd

--- client/src/app/shared/socket.service.ts
+++ client/src/app/shared/socket.service.ts
@@ -4,7 +4,7 @@ export class SocketService {
 	static instance: SocketService = null;
 	static isCreating: Boolean = false;
 	public socket: any;
-	private url = 'localhost:4200';
+	private url = 'https://node-bowshock.herokuapp.com';
 
 	/**
 	 * constuctor with control handle, that you can not instantiate by new NodoSocket();
